5bad981e31e70b0ea8a8d4065ddef63fdd1fbbe1
zomeki <zomeki@zomeki.jp>
2014-03-28 15:16:00 +0900

    Internal conversion should run only once (fixed)

--- lib/plugins/zomeki_plugin/lib/extend_rack.rb
+++ lib/plugins/zomeki_plugin/lib/extend_rack.rb
@@ -1,12 +1,16 @@
 module Rack
-  class Lock
-    def call_with_internal(env)
-      Core.initialize(env)
-      Core.recognize_path(env['PATH_INFO'])
-      env['PATH_INFO'] = Core.internal_uri
+  module Session
+    module Abstract
+      class ID
+        def call_with_internal(env)
+          Core.initialize(env)
+          Core.recognize_path(env['PATH_INFO'])
+          env['PATH_INFO'] = Core.internal_uri
 
-      call_without_internal(env)
+          call_without_internal(env)
+        end
+        alias_method_chain :call, :internal
+      end
     end
-    alias_method_chain :call, :internal
   end
 end
--- spec/lib/plugins/zomeki_plugin/lib/extend_rack_spec.rb
+++ spec/lib/plugins/zomeki_plugin/lib/extend_rack_spec.rb
@@ -1,29 +1,8 @@
 require 'spec_helper'
 
-# see rack-1.5.2/test/spec_lock.rb
+# see rack-1.5.2/lib/rack/session/abstract/id.rb
 
-class Lock
-  attr_reader :synchronized
-
-  def initialize
-    @synchronized = false
-  end
-
-  def synchronize
-    @synchronized = true
-    yield
-  end
-
-  def lock
-    @synchronized = true
-  end
-
-  def unlock
-    @synchronized = false
-  end
-end
-
-describe Rack::Lock do
+describe Rack::Session::Abstract::ID do
   before do
     Core.user = FactoryGirl.create(:sys_user_system_admin)
     Core.user_group = Core.user.group
@@ -33,12 +12,9 @@ describe Rack::Lock do
 
   describe '#call' do
     it 'replaces PATH_INFO to internal' do
-      lock = Lock.new
-      res = ['Hello World']
-      app = Rack::Lock.new(lambda { |inner_env| [200, {"Content-Type" => "text/plain"}, res] }, lock)
-
       env = Rack::MockRequest.env_for('http://zomeki.example.com/abc')
-      app.call(env)
+      id = Rack::Session::Abstract::ID.new(->(inner_env) { [200, {'Content-Type' => 'text/plain'}, ['Hello World']] }, env)
+      id.call(env)
 
       expect(env['PATH_INFO']).to eq '/_public/cms/node_pages/abc'
     end
