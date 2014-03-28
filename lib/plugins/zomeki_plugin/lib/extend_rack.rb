1d6fdaa32bb0aa2a00b1b9bbdf72d7bfb5d26266
zomeki <zomeki@zomeki.jp>
2014-03-28 11:07:24 +0900

    Internal conversion should run only once

--- lib/plugins/zomeki_plugin/lib/extend_rack.rb
+++ lib/plugins/zomeki_plugin/lib/extend_rack.rb
@@ -1,9 +1,11 @@
 module Rack
   class Request
     def initialize_with_internal(env)
-      Core.initialize(env)
-      Core.recognize_path(env['PATH_INFO'])
-      env['PATH_INFO'] = Core.internal_uri
+      unless Core.internal_uri == env['PATH_INFO']
+        Core.initialize(env)
+        Core.recognize_path(env['PATH_INFO'])
+        env['PATH_INFO'] = Core.internal_uri
+      end
 
       initialize_without_internal(env)
     end
