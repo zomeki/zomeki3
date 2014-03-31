df863bd361404a05ee9a1d5620003c460ddebdfa
zomeki <zomeki@zomeki.jp>
2014-03-31 11:41:13 +0900

    Change rspec generator option

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module ZomekiCMS
 
     config.generators do |g|
       g.test_framework :rspec,
-        fixture: true,
+        fixtures: true,
         view_specs: false,
         helper_specs: false,
         routing_specs: false,
