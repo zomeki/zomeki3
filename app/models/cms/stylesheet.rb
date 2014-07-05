63d961061aa86a3f2e0c27061fd0dd8068b98123
zomeki <zomeki@zomeki.jp>
2014-07-06 00:02:57 +0900

    Replace regexp from '^$' to '\A\z' by security reason

--- app/models/cms/stylesheet.rb
+++ app/models/cms/stylesheet.rb
@@ -6,7 +6,7 @@ class Cms::Stylesheet < Sys::Model::ValidationModel::Base
   attr_accessor :new_directory, :new_file, :new_upload
   
   validates_presence_of :name
-  validates_format_of :name, :with=> /^[0-9A-Za-z@\.\-\_]+$/, :message=> :not_a_filename
+  validates_format_of :name, :with=> /\A[0-9A-Za-z@\.\-\_]+\z/, :message=> :not_a_filename
   
   def initialize(full_path, params = {})
     @full_path  = full_path
