1bc71213c80b65d216ec69dc18ed00b8de3829b1
zomeki <zomeki@zomeki.jp>
2014-07-09 00:40:40 +0900

    Change route to response thread

--- config/modules/bbs/routes.rb
+++ config/modules/bbs/routes.rb
@@ -29,6 +29,6 @@ ZomekiCMS::Application.routes.draw do
     match "node_threads/(index.:format)" => "public/node/threads#index", via: [:get, :post]
     get "node_threads/new" => "public/node/threads#new"
     get "node_threads/delete" => "public/node/threads#delete"
-    get "node_threads/:thread/(index.:format)" => "public/node/threads#show"
+    match "node_threads/:thread/(index.:format)" => "public/node/threads#show", via: [:get, :post]
   end
 end
