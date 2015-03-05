023dcf482278e5eeb9f4814ada73dd06af67d702
mmukai <mukai@idsinc.co.jp>
2015-03-05 17:53:52 +0900

    Add pickup article piece to Cms

--- config/modules/cms/routes.rb
+++ config/modules/cms/routes.rb
@@ -163,6 +163,11 @@ CmsCMS::Application.routes.draw do
       :path        => ":piece/piece_link_items"
     resources :piece_sns_parts,
       :controller  => "admin/piece/sns_parts"
+    resources(:piece_pickup_docs,
+      :controller => 'admin/piece/pickup_docs') do
+      resources :docs,
+        :controller => 'admin/piece/pickup_docs/docs'
+    end
   end
 
   scope "#{CmsCMS::ADMIN_URL_PREFIX}/#{mod}", :module => mod, :as => '' do
