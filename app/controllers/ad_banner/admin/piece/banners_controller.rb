bd60ebca2df725c4840e7c90da3da2b085af8ca7
zomeki <zomeki@zomeki.jp>
2015-03-14 22:39:14 +0900

    Fix params for strong_parameters

--- app/controllers/ad_banner/admin/piece/banners_controller.rb
+++ app/controllers/ad_banner/admin/piece/banners_controller.rb
@@ -1,2 +1,9 @@
 class AdBanner::Admin::Piece::BannersController < Cms::Admin::Piece::BaseController
+  private
+
+  def base_params
+    params.require(:item).permit(:body, :concept_id, :name, :state, :title, :view_title,
+                                 in_creator: [:group_id, :user_id],
+                                 in_settings: [:group_id, :sort, :upper_text, :lower_text])
+  end
 end
