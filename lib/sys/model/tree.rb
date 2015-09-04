9593ab6ca50e9133bab0a8cadfa225862f0b109f
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2015-09-04 13:03:14 +0900

    Add tree methods

--- lib/sys/model/tree.rb
+++ lib/sys/model/tree.rb
@@ -5,20 +5,26 @@ module Sys::Model::Tree
     climb_parents_tree(id, :class => self.class)
   end
 
+  def root?
+    parent_id.in?([0, nil])
+  end
+
   def ancestors(items = [])
     parent.ancestors(items) if parent
     items << self
   end
 
-  def descendants(items = [])
+  def descendants(items = [], &block)
     items << self
-    children.each {|c| c.descendants(items) }
+    rel = children
+    rel = yield(rel) || rel if block_given?
+    rel.each {|c| c.descendants(items, &block) }
     items
   end
 
   module ClassMethods
     def roots
-      self.where([0, nil].map{|v| arel_table[:parent_id].eq(v)}.reduce(:or))
+      self.where(parent_id: [0, nil])
     end
 
     def root
