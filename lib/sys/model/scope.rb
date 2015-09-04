95cd646114a8b70f26cb014e9617d7c379e4a366
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2015-09-04 17:10:52 +0900

    Tweak scope method

--- lib/sys/model/scope.rb
+++ lib/sys/model/scope.rb
@@ -5,7 +5,13 @@ module Sys::Model::Scope
     scope :search_with_text, ->(*args) {
       words = args.pop.to_s.split(/[ 　]+/)
       columns = args
-      where(words.map{|w| columns.map{|c| arel_table[c].matches("%#{w.gsub(/([_%])/,'\\\\\1')}%") }.reduce(:or) }.reduce(:and))
+      where(words.map{|w| columns.map{|c| arel_table[c].matches("%#{escape_like(w)}%") }.reduce(:or) }.reduce(:and))
     }
+  end
+
+  module ClassMethods
+    def escape_like(s)
+      s.gsub(/[\\%_]/) {|r| "\\#{r}"}
+    end
   end
 end
