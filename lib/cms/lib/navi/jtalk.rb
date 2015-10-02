324f144dc3793d570fe9d8d920018169f4e190de
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2015-10-02 11:24:29 +0900

    Improve text conversion for talk

--- lib/cms/lib/navi/jtalk.rb
+++ lib/cms/lib/navi/jtalk.rb
@@ -1,78 +1,8 @@
 # encoding: utf-8
+require 'MeCab'
+require "cgi"
+require "kconv"
 class Cms::Lib::Navi::Jtalk
-
-  def self.make_text(html, site_id=nil)
-    require 'MeCab'
-    require "cgi"
-    require "kconv"
-    
-    ## settings
-    mecab_rc = Cms::KanaDictionary.mecab_rc(site_id)
-
-    doc = Nokogiri::HTML(html.toutf8, nil, 'utf-8')
-    if content = doc.xpath('//div[@id="content"]')
-      content_html = content.children
-    end
-    if body = doc.xpath('//body')
-      body_html = body.children
-    end
-    
-    if !body_html.blank? && !content_html.blank?
-      html = content_html.blank? ? body_html : content_html
-      html = html.to_html
-    end
-    
-    ## trim
-    html.gsub!(/(\r\n|\r|\n)+/, " ")
-
-    if html =~ /<!-- skip.reading -->/
-      html.gsub!(/<!-- skip.reading -->.*?<!-- \/skip.reading -->/i, '')
-    end
-    ["style", "script", "noscript", "iframe", "rb", "rp"].each do |name|
-      html.gsub!(/<#{name}.*?>.*?<\/#{name}>/i, '') if html =~ /<#{name}/
-    end
-
-    ## img
-    html.gsub!(/<img .*?>/i) do |m|
-      alt = nil
-      alt = m.sub(/.* title="(.*?)".*/i, '\\1') if m =~ / title=".*?"/
-      alt = m.sub(/.* alt="(.*?)".*/i, '\\1') if m =~ / alt=".*?"/
-      alt ? "画像 #{alt}" : ""
-    end
-
-    html.gsub!(/<\/(h1|h2|h3|h4|h5|p|div|pre|blockquote|ul|ol)>/i, "。")
-    html.gsub!(/<\/?[a-z!][^>]*>/i, "")
-
-    html = CGI::unescapeHTML(html)
-    html.gsub!("&nbsp;", " ")
-    html.gsub!(/[\s\t\v\n、，　「」【】（）\(\)<>\[\]]+/, " ")
-    html.gsub!(/\s*。+\s*/, "。")
-    html.gsub!(/。+/, "。")
-    html.tr!('０-９ａ-ｚＡ-Ｚ', '0-9a-zA-Z')
-    html.gsub!(/^[、。 ]+/, "")
-    html.gsub!(/[、。]+$/, "")
-
-    texts = []
-
-    mc = MeCab::Tagger.new('--node-format=%c,%M,%H\n -r ' + mecab_rc)
-    mc.parse(html).split(/\n/).each_with_index do |line, line_no|
-      p = line.split(/,/)
-      next if line == "EOS"
-
-      cost = p[0]
-      word = p[1]
-      kana = p[9]
-
-      if !kana || kana == "*" || cost != "100"
-        texts << word # skip
-      elsif word == kana.tr('ァ-ン', 'ぁ-ん')
-        texts << word
-      else
-        texts << kana
-      end
-    end
-    texts.join
-  end
 
   def make(*args)
     ## settings
@@ -158,4 +88,86 @@ class Cms::Lib::Navi::Jtalk
     return nil
   end
 
+  class << self
+    def make_text(html, site_id = nil)
+      text = html_to_text(html)
+      apply_kana_dic(text, site_id)
+    end
+
+    private
+
+    def html_to_text(html)
+      doc = Nokogiri::HTML(html.toutf8, nil, 'utf-8')
+
+      content = doc.css('div[@id="content"]').first || doc.css('body').first || doc.root
+      return '' unless content
+
+      content.xpath('.//comment()[.=" skip reading "]').each do |comment1|
+        if comment2 = comment1.xpath('following-sibling::comment()[.=" /skip reading "]').first
+          nodes = nodes_between(comment1, comment2)
+          nodes.each(&:remove) if nodes.last == comment2
+        end
+      end
+
+      ## replace img tag
+      content.css('img').each do |img|
+        if alt = img['alt'].presence || img['title'].presence
+          img.replace(Nokogiri::XML::Text.new("画像 #{alt}", doc))
+        else
+          img.remove
+        end
+      end
+
+      ## remove unnecessary tags
+      content.css('style, script, noscript, iframe, rb, rp').remove
+
+      ## make end of sentence
+      content.css('h1, h2, h3, h4, h5, p, div, pre, blockquote, ul, ol').each do |node|
+        node.next = Nokogiri::XML::Text.new('。', doc)
+      end
+
+      ## convert to text
+      html = content.text
+
+      ## trim
+      html.gsub!(/(\r\n|\r|\n)+/, " ")
+      html.gsub!(/[\s\t\v\n、，　「」【】（）\(\)<>\[\]]+/, " ")
+      html.gsub!(/\s*。+\s*/, "。")
+      html.gsub!(/。+/, "。")
+      html.tr!('０-９ａ-ｚＡ-Ｚ', '0-9a-zA-Z')
+      html.gsub!(/^[、。 ]+/, "")
+      html.gsub!(/[、。]+$/, "")
+      html
+    end
+
+    def nodes_between(first, last)
+      !first ? [] : first == last ? [last] : [first, *nodes_between(first.next, last)]
+    end
+
+    def apply_kana_dic(text, site_id = nil)
+      ## settings
+      mecab_rc = Cms::KanaDictionary.mecab_rc(site_id)
+
+      texts = []
+
+      mc = MeCab::Tagger.new('--node-format=%c,%M,%H\n -r ' + mecab_rc)
+      mc.parse(text).split(/\n/).each_with_index do |line, line_no|
+        p = line.split(/,/)
+        next if line == "EOS"
+
+        cost = p[0]
+        word = p[1]
+        kana = p[9]
+
+        if !kana || kana == "*" || cost != "100"
+          texts << word # skip
+        elsif word == kana.tr('ァ-ン', 'ぁ-ん')
+          texts << word
+        else
+          texts << kana
+        end
+      end
+      texts.join
+    end
+  end
 end
