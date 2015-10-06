ae5d22c89b2559906efbe816a0f022836a1b14fd
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2015-10-06 16:28:51 +0900

    Refactor kana conversion method

--- lib/cms/lib/navi/kana.rb
+++ lib/cms/lib/navi/kana.rb
@@ -8,35 +8,29 @@ class Cms::Lib::Navi::Kana
       html = html.to_utf8.gsub(/\r\n/, "\n")
       tmp = mask_html(html)
 
-      bytes = html.unpack("C*")
       texts = []
       pos   = 0
 
       require 'MeCab'
       mecab_rc = Cms::KanaDictionary.mecab_rc(site_id)
-      mc = MeCab::Tagger.new('--node-format=%ps,%pe,%M,%f[7]\n --unk-format= --eos-format= -r ' + mecab_rc)
+      mc = MeCab::Tagger.new('--node-format=%ps,%pe,%m,%f[7]\n --unk-format= --eos-format= -r ' + mecab_rc)
       mc.parse(tmp).split("\n").each do |line|
-        p = line.split(",")
-        next if p[0] !~ /^[0-9]+$/
-        next if p[2] =~ /^[\*\.]+$/
-        next if p[3].blank?
-        next if p[2] == p[3]
-        next if p[2] !~ /[一-龠]/
-        kana = p[3].to_s.tr('ァ-ン', 'ぁ-ん')
-        next if p[2] == kana
+        s, e, word, kana = line.split(",")
+        next if s !~ /^[0-9]+$/
+        next if word !~ /[一-龠]/
+        next if kana.blank?
 
-        s = p[0].to_i
-        e = p[1].to_i
-
-        texts << bytes.slice(pos..s-1).pack("C*").force_encoding('utf-8') if pos < s
-
-        word = bytes.slice(s..e-1).pack("C*").force_encoding('utf-8')
+        s = s.to_i
+        e = e.to_i
+        kana = kana.to_s.tr('ァ-ン', 'ぁ-ん')
+ 
+        texts << html.byteslice(pos..s-1) if pos < s
         texts << "<ruby><rb>#{word}</rb><rp>(</rp><rt>#{kana}</rt><rp>)</rp></ruby>"
 
         pos = e
       end
 
-      texts << bytes.slice(pos..-1).pack("C*").force_encoding('utf-8') if pos < bytes.size
+      texts << html.byteslice(pos..-1) if pos < html.bytesize
       texts.join.html_safe
     end
 
@@ -51,9 +45,5 @@ class Cms::Lib::Navi::Kana
       end
       tmp.gsub!(/<[^>]+>/, &mask)
     end
-
-    # def self.slice(str, start, length)
-      # "#{str}".force_encoding('ascii').slice(start, length).to_s.force_encoding('utf-8')
-    # end
   end
 end
