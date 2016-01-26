404a89fdb31cf17218f4547196592a0290236cbf
tao-k <tao@sitebridge.co.jp>
2016-01-26 10:12:27 +0900

    add option to application.yml
    allow_japanese_filename

--- app/models/cms/data_file_node.rb
+++ app/models/cms/data_file_node.rb
@@ -7,14 +7,14 @@ class Cms::DataFileNode < ActiveRecord::Base
   include Cms::Model::Rel::Site
   include Cms::Model::Rel::Concept
   include Cms::Model::Auth::Concept
-  
+
   has_many :files, :foreign_key => :node_id, :class_name => 'Cms::DataFile', :primary_key => :id
-  
+
   validates :name, presence: true, uniqueness: { scope: :concept_id }
   validate :validate_name
-  
+
   after_destroy :remove_files
-  
+
   def label(separator = " : ")
     label = name
     unless title.blank?
@@ -22,15 +22,16 @@ class Cms::DataFileNode < ActiveRecord::Base
     end
     label
   end
-  
+
   def validate_name
+    return if Zomeki.config.application['upload.allow_japanese_filename']
     if !name.blank?
       if name !~ /^[0-9a-zA-Z_\-]+$/
         errors.add :name, "は半角英数字を入力してください。"
       end
     end
   end
-  
+
 private
   def remove_files
     files.each {|file| file.destroy }
