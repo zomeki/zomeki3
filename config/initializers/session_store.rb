b3cca5f4729dba3f26d3e77f273a8f48b8895555
MIKAMI Yoshiyuki <yoshuki@saikyoline.jp>
2016-05-27 11:55:19 +0900

    Execute rake rails:update

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-ZomekiCMS::Application.config.session_store :cookie_store, key: '_cms_cms_session'
+Rails.application.config.session_store :cookie_store, key: '_zomeki_cms_session'
