cf8f0cf0b5ac6d21948a85c9b81c1b51ca3f8789
MIKAMI Yoshiyuki <yoshuki@saikyoline.jp>
2016-07-23 14:26:05 +0900

    Add nginx install script

---
+++ doc/install_scripts/install_nginx.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+DONE_FLAG="/tmp/$0_done"
+
+echo "#### Install nginx ####"
+if [ -f $DONE_FLAG ]; then exit; fi
+echo '-- PRESS ENTER KEY --'
+read KEY
+
+centos() {
+  echo "It's CentOS!"
+
+cat <<'EOF' > /etc/yum.repos.d/nginx.repo
+[nginx]
+name=nginx repo
+baseurl=http://nginx.org/packages/centos/7/$basearch/
+gpgcheck=0
+enabled=1
+EOF
+
+  yum -y install nginx
+}
+
+others() {
+  echo 'This OS is not supported.'
+}
+
+if [ -f /etc/centos-release ]; then
+  centos
+elif [ -f /etc/lsb-release ]; then
+  if grep -qs Ubuntu /etc/lsb-release; then
+    echo 'Ubuntu will be supported shortly.'
+  else
+    others
+  fi
+else
+  others
+fi
+
+touch $DONE_FLAG
