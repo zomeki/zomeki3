7a7661692adf18afa4508719f7ce96a7585a2d30
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2016-08-18 18:13:17 +0900

    Fix editable method

--- lib/sys/model/auth/editable_group.rb
+++ lib/sys/model/auth/editable_group.rb
@@ -1,26 +1,28 @@
-# encoding: utf-8
 module Sys::Model::Auth::EditableGroup
-  def editable
-    return self if Core.user.has_auth?(:manager)
-    
-    self.join :editable_group
-    self.and Condition.new do |c|
-      col = "sys_editable_groups.group_ids"
-      val = Core.user_group.id
-      c.or col, 'REGEXP', "(^| )#{val}( |$)"
-      
-      join_creator
-      c.or "sys_creators.group_id", Core.user_group.id 
-    end
-    return self
+  extend ActiveSupport::Concern
+
+  included do
+    scope :editable, -> {
+      if Core.user.has_auth?(:manager)
+        all
+      else
+        creators = Sys::Creator.arel_table
+        editable_groups = Sys::EditableGroup.arel_table
+        gid = Core.user_group.id
+        distinct.joins(:editable_group, :creator).where([
+          creators[:group_id].eq(Core.user_group.id),
+          Arel.sql("sys_editable_groups.group_ids ~ '(^| )#{gid}( |$)'"),
+        ].reduce(:or))
+      end
+    }
   end
-  
+
   def creatable?
     return false unless Core.user.has_auth?(:creator)
     #return Core.user.has_priv?(:create, :item => content.concept)
     return true
   end
-  
+
   def editable?
     return true if Core.user.has_auth?(:manager)
     return false unless creator
@@ -28,7 +30,7 @@ module Sys::Model::Auth::EditableGroup
     return false unless editable_group
     return editable_group.group_ids =~ /(^| )#{Core.user_group.id}( |$)/
   end
-  
+
   def deletable?
     editable?
   end
