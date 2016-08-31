eef9b73add6b78e93171b2b074c7baf361a46eb1
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2016-08-31 13:46:10 +0900

    Refactor gp_article mailer

--- app/mailers/common_mailer.rb
+++ app/mailers/common_mailer.rb
@@ -5,18 +5,4 @@ class CommonMailer < ApplicationMailer
          to: options[:to],
          subject: options[:subject]
   end
-
-  def commented_notification(comment)
-    @doc = comment.doc
-    @core_uri = Cms::SiteSetting::AdminProtocol.core_domain @doc.content.site, @doc.content.site.full_uri
-
-    from = comment.author_name
-    from << " <#{comment.author_email}>" if comment.author_email.present?
-
-    return if from.blank? || @doc.creator.user.email.blank?
-
-    mail from: from,
-         to: @doc.creator.user.email,
-         subject: "#{@doc.content.name}（#{@doc.content.site.name}）：コメント通知メール"
-  end
 end
