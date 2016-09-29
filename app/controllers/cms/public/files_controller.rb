8c1f00384ca7e92cf236919ca827be15aeaea1aa
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2016-09-29 13:19:40 +0900

    Fix thumbnail preview for data_files

--- app/controllers/cms/public/files_controller.rb
+++ app/controllers/cms/public/files_controller.rb
@@ -1,19 +1,28 @@
 class Cms::Public::FilesController < ApplicationController
   def down
-    return http_error(404) if params[:path] !~ /^[^\/]+\/[^\/]+$/
-    id   = params[:path].gsub(/\/.*/, '')
-    name = params[:path].gsub(/.*\//, '') + '.' + params[:format].to_s
+    paths = params[:path].to_s.split('/')
+    return http_error(404) if paths.size != 2 && paths.size != 3
 
-    item = Cms::DataFile.public_state.where(id: id.gsub(/.\z/, ''), name: name).first
+    id    = paths[0]
+    name  = "#{paths.last}.#{params[:format]}"
+    type  = paths.size == 3 ? paths[1] : nil
+    return http_error(404) if id !~ /^[0-9]+$/
+
+    item = Cms::DataFile.public_state.where(id: id.chop, name: name).first
     return http_error(404) unless item
 
     path = item.public_path
+    path = ::File.dirname(path) + "/#{type}/#{name}" if type
     return http_error(404) unless FileTest.exist?(path)
-    
-    if img = item.mobile_image(request.mobile, :path => item.public_path)
-      return send_data(img.to_blob, :type => item.mime_type, :filename => item.name, :disposition => 'inline')
+
+    if type
+      return send_file(path, type: item.mime_type, filename: item.name)
     end
-    
-    return send_file(path, :type => item.mime_type, :filename => item.name, :disposition => 'inline')
+
+    if img = item.mobile_image(request.mobile, path: item.public_path)
+      return send_data(img.to_blob, type: item.mime_type, filename: item.name, disposition: 'inline')
+    end
+
+    return send_file(path, type: item.mime_type, filename: item.name, disposition: 'inline')
   end
 end
