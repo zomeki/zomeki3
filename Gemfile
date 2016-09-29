0f6109911388a295a18060fd864e724d949a3d8a
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2016-09-29 15:21:14 +0900

    Change puma to unicorn

--- Gemfile
+++ Gemfile
@@ -25,10 +25,10 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # gem 'bcrypt', '~> 3.1.7'
 
 # Use Puma as the app server
-gem 'puma'
+# gem 'puma'
 
 # Use Unicorn as the app server
-# gem 'unicorn'
+gem 'unicorn'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
---
+++ config/unicorn/development.rb
@@ -0,0 +1,39 @@
+rails_root = File.expand_path('../../../', __FILE__)
+
+rails_env = ENV['RAILS_ENV'] || "production"
+
+ENV['BUNDLE_GEMFILE'] = rails_root + "/Gemfile"
+
+working_directory rails_root
+
+timeout 30
+
+preload_app false
+
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 2)
+
+# listen 8080, backlog: 1024
+listen "#{rails_root}/tmp/sockets/unicorn.sock", backlog: 1024
+
+pid "#{rails_root}/tmp/pids/unicorn.pid"
+
+stderr_path "#{rails_root}/log/#{rails_env}_unicorn_stderr.log"
+stdout_path "#{rails_root}/log/#{rails_env}_unicorn_stdout.log"
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+      ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
