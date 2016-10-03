2f6dd09e816c753d398e7d39c32956d8fd61bedc
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2016-10-03 17:56:43 +0900

    Fix scaffold alert message on _create method

--- lib/sys/controller/scaffold/base.rb
+++ lib/sys/controller/scaffold/base.rb
@@ -33,7 +33,7 @@ protected
       end
     else
       failed_template = options[:failed_template] || {:action => :new}
-      flash[:alert] = '登録処理に失敗しました。'
+      flash.now[:alert] = '登録処理に失敗しました。'
       respond_to do |format|
         format.html { render failed_template }
         format.xml  { render :xml => item.errors, :status => :unprocessable_entity }
