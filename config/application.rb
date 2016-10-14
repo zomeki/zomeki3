e6502be5635a61a69a107bc8a0baa6e58b10a1a0
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2016-10-14 14:12:03 +0900

    Fix DEPRECATION WARNING: ActiveRecord::Base.raise_in_transactional_callbacks= is deprecated

--- config/application.rb
+++ config/application.rb
@@ -24,9 +24,6 @@ module ZomekiCMS
     # config.i18n.default_locale = :de
     config.i18n.default_locale = :ja
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-
     # Custom directories with classes and modules you want to be autoloadable.
     #config.autoload_paths += %W(#{config.root}/lib)
     config.eager_load_paths += %W(#{config.root}/lib)
