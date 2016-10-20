8f541d1fbd02523aaac32b9f44a9c0e505144b6e
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2016-10-20 14:48:12 +0900

    [zomekiv3-sqr:00085] Fix rewrite for _mobile and _smartphone

--- config/initializers/app/initializer.rb
+++ config/initializers/app/initializer.rb
@@ -4,13 +4,20 @@ module Cms
       def initialize(app)
         @app = app
       end
-    
+
       def call(env)
+        rewrite(env)
         Core.initialize(env)
         Core.recognize_path(env['PATH_INFO'])
         env['PATH_INFO'] = Core.internal_uri
-    
+
         @app.call(env)
+      end
+
+      private
+
+      def rewrite(env)
+        env['PATH_INFO'].gsub!(%r{/_(mobile|smartphone)}, '')
       end
     end
   end
