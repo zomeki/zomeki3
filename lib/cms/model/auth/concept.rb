753c5bd2c939587491fa833082382b8370d3afe1
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2016-10-21 15:34:42 +0900

    Fix warning: Creating scope :readable. Overwriting existing method

--- lib/cms/model/auth/concept.rb
+++ lib/cms/model/auth/concept.rb
@@ -1,22 +1,6 @@
 module Cms::Model::Auth::Concept
   extend ActiveSupport::Concern
 
-  included do
-    scope :readable, -> {
-      rel = Core.site ? where(site_id: Core.site.id)
-                      : where(site_id: nil)
-      if Core.concept
-        if Core.user.has_priv?(:read, item: Core.concept)
-          rel.where(concept_id: Core.concept.id)
-        else
-          rel.none
-        end
-      else
-        rel.where(concept_id: nil)
-      end
-    }
-  end
-
   def creatable?
     return false unless Core.user.has_auth?(:designer)
     return Core.user.has_priv?(:create, item: concept)
@@ -35,5 +19,21 @@ module Cms::Model::Auth::Concept
   def deletable?
     return false unless Core.user.has_auth?(:designer)
     return Core.user.has_priv?(:delete, item: concept)
+  end
+
+  class_methods do
+    def readable
+      rel = Core.site ? where(site_id: Core.site.id)
+                      : where(site_id: nil)
+      if Core.concept
+        if Core.user.has_priv?(:read, item: Core.concept)
+          rel.where(concept_id: Core.concept.id)
+        else
+          rel.none
+        end
+      else
+        rel.where(concept_id: nil)
+      end
+    end
   end
 end
--- lib/sys/model/auth/designer.rb
+++ lib/sys/model/auth/designer.rb
@@ -1,11 +1,6 @@
 module Sys::Model::Auth::Designer
   extend ActiveSupport::Concern
 
-  included do
-    scope :readable, -> { Core.user.has_auth?(:designer) ? all : none }
-    scope :editable, -> { Core.user.has_auth?(:designer) ? all : none }
-  end
-
   def creatable?
     Core.user.has_auth?(:designer)
   end
@@ -20,5 +15,15 @@ module Sys::Model::Auth::Designer
 
   def deletable?
     Core.user.has_auth?(:designer)
+  end
+
+  class_methods do
+    def readable
+      Core.user.has_auth?(:designer) ? all : none
+    end
+
+    def editable
+      Core.user.has_auth?(:designer) ? all : none
+    end
   end
 end
