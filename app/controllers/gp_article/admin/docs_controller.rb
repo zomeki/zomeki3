7d5e5309b2f059c5acc1341d8481b850d867d6ac
tao-k <tao@sitebridge.co.jp>
2016-10-24 17:53:40 +0900

    [zomekiv3-sqr:00111]
    fix related docs select

--- app/controllers/gp_article/admin/docs_controller.rb
+++ app/controllers/gp_article/admin/docs_controller.rb
@@ -256,16 +256,6 @@ class GpArticle::Admin::DocsController < Cms::Controller::Admin::Base
     end
   end
 
-  def select
-    @doc = {
-      id: @item.id, title: @item.title, full_uri: @item.state_public? ? @item.public_full_uri : nil,
-      name: @item.name, content_id: @item.content_id,
-      updated: @item.updated_at.strftime('%Y/%m/%d %H:%M'), status: @item.status.name,
-      user: @item.creator.user.try(:name), group: @item.creator.group.try(:name)
-    }
-    _show @doc
-  end
-
   protected
 
   def select_layout
