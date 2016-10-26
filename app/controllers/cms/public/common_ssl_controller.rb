65700bf318c36c82a380215220581ce7d0f60501
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2016-10-26 14:03:25 +0900

    Remove unused method

--- app/controllers/cms/public/common_ssl_controller.rb
+++ app/controllers/cms/public/common_ssl_controller.rb
@@ -1,6 +1,4 @@
 class Cms::Public::CommonSslController < ApplicationController
-#  after_action :replace_preview_data
-  
   def index
     path = Core.request_uri.gsub(/^#{Regexp.escape(cms_common_ssl_path)}/, "")
     render_ssl(path, :mobile => Page.mobile?, :smart_phone => request.smart_phone?, :preview => true)
@@ -41,52 +39,5 @@ class Cms::Public::CommonSslController < ApplicationController
 
     component_response = render_component :controller => ctl, :action => act, :params => params, :jpmobile => (Page.smart_phone? ? envs_to_request_as_smart_phone : nil)
     response.content_type = component_response.content_type unless component_response.class == String
-  end
-
-protected
-  def replace_preview_data
-    return if response.content_type != 'text/html' && response.content_type != 'application/xhtml+xml'
-    return if response.body.class != String
-
-    public_uri = URI.parse(Page.site.full_uri)
-    public_uri.path = '/'
-    if Core.script_uri
-      admin_uri = URI.parse(Core.script_uri)
-      admin_uri.path = '/'
-    else
-      admin_uri = public_uri
-    end
-    
-    mobile   = Page.mobile? ? 'm' : ''
-    base_uri = "#{admin_uri}_preview/#{format('%04d', Page.site.id)}#{mobile}"
-
-    self.response_body = response.body.gsub(/<a[^>]+?href="\/[^"]*?"[^>]*?>/i) do |m|
-      if m =~ /href="\/_(files|layouts)\//
-        m
-      else
-        m.gsub(/^(<a[^>]+?href=")(\/[^"]*?)("[^>]*?>)/i, '\\1' + base_uri + '\\2\\3')
-      end
-    end
-    
-    ## preview mark
-    html = render_to_string(partial: 'cms/admin/preview/preview_mark')
-    self.response_body = response.body.gsub(/(<body[^>]*?>)/i, '\\1' + html)
-
-    ## host
-    doc = Nokogiri::HTML.parse(response.body)
-    unless (a_tags = doc.css(%Q!a[href^="#{public_uri}"]!)).empty?
-      a_tags.each do |a_tag|
-        a_tag.set_attribute('href', a_tag.attribute('href').to_s.sub(public_uri.to_s, admin_uri.to_s))
-      end
-    end
-    unless (img_tags = doc.css(%Q!img[src^="#{public_uri}"]!)).empty?
-      img_tags.each do |img_tag|
-        img_tag.set_attribute('src', img_tag.attribute('src').to_s.sub(public_uri.to_s, admin_uri.to_s))
-      end
-    end
-    self.response_body = doc.to_s
-
-    ## themes
-    self.response_body = response.body.gsub(%r! (href|src)="/_themes/!, %Q! \\1="#{public_uri}_themes/!)
   end
 end
