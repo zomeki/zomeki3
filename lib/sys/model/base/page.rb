252623f914b64907e529a5eebbea00efc88a3f7b
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2016-10-27 13:08:18 +0900

    Remove unused method

--- lib/sys/model/base/page.rb
+++ lib/sys/model/base/page.rb
@@ -2,12 +2,7 @@ module Sys::Model::Base::Page
   def states
     [['公開','public'],['非公開','closed']]
   end
-  
-  def public
-    self.and "#{self.class.table_name}.state", 'public'
-    self
-  end
-  
+
   def public?
     return state == 'public' && !published_at.blank?
   end
