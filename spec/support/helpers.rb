f0963c47fff23d2f2346dbd6fa3c5b4b524ddd3a
MIKAMI Yoshiyuki <yoshuki@saikyoline.jp>
2016-11-17 16:10:16 +0900

    Update helpers

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -2,22 +2,16 @@ module Helpers
   def login_as(account)
     # See Sys::Controller::Admin::Auth#new_login
     user = Sys::User.find_by_account(account)
-    return nil unless user
-    if defined?(session)
-      session[Sys::Controller::Admin::Base::ACCOUNT_KEY] = user.account
-      session[Sys::Controller::Admin::Base::PASSWD_KEY] = user.encrypt_password
-    end
+# TODO: SystemStackError: stack level too deep
+#    session[Sys::Controller::Admin::Auth::ACCOUNT_KEY] = user.account
+#    session[Sys::Controller::Admin::Auth::PASSWD_KEY] = user.encrypt_password
 
     # See Sys::Controller::Admin::Base#initialize_application
     Core.user = user
     Core.user.password = Util::String::Crypt.decrypt(user.encrypt_password, Zomeki.config.application['sys.crypt_pass'])
     Core.user_group = user.group
-    return user
-  end
 
-  def fg_find_or_create(fg_id)
-    klass = FactoryGirl.build(fg_id).class
-    klass.find_by_id(FactoryGirl.attributes_for(fg_id)[:id]) || FactoryGirl.create(fg_id)
+    return user
   end
 
   def initialize_core(uri)
