d4c6e455fb02e50c142e2df04e0033075ec988fd
MIKAMI Yoshiyuki <yoshuki@saikyoline.jp>
2016-11-19 00:29:40 +0900

    Fix specs for "Approval::ApprovalFlow"

--- spec/factories/approval_content_approval_flows.rb
+++ spec/factories/approval_content_approval_flows.rb
@@ -1,11 +1,13 @@
 FactoryGirl.define do
-  factory :approval_content_approval_flow_1, class: 'Approval::Content::ApprovalFlow' do
-    site_id 1
-    concept_id 1
+  factory :approval_content_approval_flow, class: 'Approval::Content::ApprovalFlow' do
+    site_id { concept.site_id }
+    association :concept, factory: :cms_concept
     state 'public'
     model 'Approval::ApprovalFlow'
-    code 'AAF1'
-    name '承認フロー１'
-    note '承認フロー１のメモ'
+    sequence(:name) {|n| "承認フロー#{n}" }
+    xml_properties nil
+    sequence(:note) {|n| "#{name}のメモ" }
+    sequence(:code) {|n| "APPROVAL#{n}" }
+    sequence(:sort_no) {|n| n * 10 }
   end
 end
