e7c2deda66234086f9f131b95a27b42c71289888
MIKAMI Yoshiyuki <yoshuki@saikyoline.jp>
2016-11-18 16:00:50 +0900

    Fix specs for "GpArticle::Link"

--- spec/models/gp_article/link_spec.rb
+++ spec/models/gp_article/link_spec.rb
@@ -1,20 +1,14 @@
-require 'spec_helper'
+require 'rails_helper'
 
-describe GpArticle::Link do
+RSpec.describe GpArticle::Link, type: :model do
   it 'has a valid factory' do
-    link = FactoryGirl.build(:gp_article_link_1)
+    link = build(:gp_article_link)
     expect(link).to be_valid
   end
 
   it 'is invalid without a doc' do
-    link = FactoryGirl.build(:gp_article_link_1, doc: nil)
-    expect(link).not_to be_valid
-    expect(link.errors[:doc_id].size).to eq(1)
-  end
-
-  it 'is invalid without a url' do
-    link = FactoryGirl.build(:gp_article_link_1, url: nil)
-    expect(link).not_to be_valid
-    expect(link.errors[:url].size).to eq(1)
+    link = build(:gp_article_link, doc: nil)
+    link.validate
+    expect(link.errors[:doc_id].size).to eq 1
   end
 end
