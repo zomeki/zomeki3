5312a293f2e54c3bfc184a3421fde39674df1ec1
tao-k <tao@sitebridge.co.jp>
2016-11-18 18:39:16 +0900

    [zomekiv3-sqr:00200][zomekiv3-sqr:00201]

--- app/models/gp_article/piece/doc.rb
+++ app/models/gp_article/piece/doc.rb
@@ -1,5 +1,7 @@
 class GpArticle::Piece::Doc < Cms::Piece
-  DOCS_ORDER_OPTIONS = [['公開日（降順）', 'published_at_desc'], ['公開日（昇順）', 'published_at_asc'], ['ランダム', 'random']]
+  DOCS_ORDER_OPTIONS = [['公開日（降順）', 'published_at_desc'], ['公開日（昇順）', 'published_at_asc'],
+                        ['更新日（降順）', 'updated_at_desc'], ['更新日（昇順）', 'updated_at_asc'],
+                        ['ランダム', 'random']]
 
   default_scope { where(model: 'GpArticle::Doc') }
 
