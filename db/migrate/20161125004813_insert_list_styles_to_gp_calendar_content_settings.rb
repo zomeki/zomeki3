8e17f13eb68a513c736ea773105daf2273ce0993
tao-k <tao@sitebridge.co.jp>
2016-11-25 10:38:03 +0900

    [zomekiv3-spec:00076]

--- db/migrate/20161125004813_insert_list_styles_to_gp_calendar_content_settings.rb
+++ db/migrate/20161125004813_insert_list_styles_to_gp_calendar_content_settings.rb
@@ -3,14 +3,9 @@ class InsertListStylesToGpCalendarContentSettings < ActiveRecord::Migration[5.0]
     calendars = GpCalendar::Content::Event.all
     calendars.each do |calendar|
       defalut_table_style = []
-      list_style = calendar.list_style.to_s || '@title_link@'
       defalut_table_style << {header: 'サムネイル', data: '@image@'} if calendar.show_images?
       defalut_table_style << {header: '開催日', data: '@hold_date'}
-      defalut_table_style << {header: 'タイトル', data: list_style}
-
-      calendar_list = GpCalendar::Content::Setting.config(calendar, 'calendar_list_style')
-      calendar_list.value = list_style
-      calendar_list.save
+      defalut_table_style << {header: 'タイトル', data: '@title_link@'}
 
       list = GpCalendar::Content::Setting.config(calendar, 'list_style')
       list.value = defalut_table_style
