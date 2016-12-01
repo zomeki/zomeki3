198d0cd9a8d349d2af1f402fd4f6e619800bb006
tao-k <tao@sitebridge.co.jp>
2016-12-01 19:58:30 +0900

    [zomekiv3-dev:00224]

--- app/helpers/gp_calendar/event_helper.rb
+++ app/helpers/gp_calendar/event_helper.rb
@@ -112,7 +112,8 @@ private
   def category_from_category_type(event, category_type_name)
     category_type = GpCategory::CategoryType.where(name: category_type_name).first
     if category_type
-      event.categories.where(category_type_id: category_type)
+      category_ids = event.categories.map{|c| c.id }
+      GpCategory::Category.where(category_type_id: category_type, id: category_ids)
     else
       nil
     end
