43db256184615cf9831e827eb8454ace752d5d72
tao-k <tao@sitebridge.co.jp>
2016-12-13 18:06:36 +0900

    [zomekiv3-sqr:00263]

--- app/controllers/organization/script/groups_controller.rb
+++ app/controllers/organization/script/groups_controller.rb
@@ -11,6 +11,8 @@ class Organization::Script::GroupsController < Cms::Controller::Script::Publicat
       g_uri = group.public_uri
       g_path = group.public_path
       g_smart_phone_path = group.public_smart_phone_path
+      publish_page(@node, uri: "#{g_uri}index.rss", path: "#{g_path}index.rss", dependent: "#{g_path}rss")
+      publish_page(@node, uri: "#{g_uri}index.atom", path: "#{g_path}index.atom", dependent: "#{g_path}atom")
       publish_more(@node, uri: g_uri, path: g_path, smart_phone_path: g_smart_phone_path, dependent: g_uri)
     end
 
