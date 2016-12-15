40be857758e4140f2fec2ecd0055b7dee388c1e8
tao-k <tao@sitebridge.co.jp>
2016-12-15 17:09:30 +0900

    [zomekiv3-sqr:00274]

--- app/models/gp_article/content/doc.rb
+++ app/models/gp_article/content/doc.rb
@@ -3,6 +3,8 @@ class GpArticle::Content::Doc < Cms::Content
 
   has_one :public_node, -> { public_state.where(model: 'GpArticle::Doc').order(:id) },
     foreign_key: :content_id, class_name: 'Cms::Node'
+  has_one :doc_node, -> { where(model: 'GpArticle::Doc').order(:id) },
+    foreign_key: :content_id, class_name: 'Cms::Node'
   has_one :public_archives_node, -> { public_state.where(model: 'GpArticle::Archive').order(:id) },
     foreign_key: :content_id, class_name: 'Cms::Node'
 
