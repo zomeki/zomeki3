05b843bcc6eba641ae84e91538dafbb39cae1d6f
tao-k <tao@sitebridge.co.jp>
2016-12-16 13:24:04 +0900

    [zomekiv3-sqr:00293]

--- app/models/cms/publisher.rb
+++ app/models/cms/publisher.rb
@@ -35,7 +35,7 @@ class Cms::Publisher < ApplicationRecord
       return if items.blank?
       pubs = items.map do |item|
         pub = self.new(site_id: site_id, publishable: item, state: 'queued', extra_flag: extra_flag)
-        pub.run_callbacks(:create) { false }
+        pub.run_callbacks(:create)
         pub
       end
       self.import(pubs)
