844ae28189fed2720de14948bf61243313c0151b
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2017-01-30 10:35:36 +0900

    [zomekiv3-spec:00109] Add category type setting for search_docs

--- app/models/gp_article/piece/search_doc.rb
+++ app/models/gp_article/piece/search_doc.rb
@@ -3,6 +3,29 @@ class GpArticle::Piece::SearchDoc < Cms::Piece
 
   after_initialize :set_default_settings
 
+  def content
+    GpArticle::Content::Doc.find(super.id)
+  end
+
+  def category_types_for_option
+    content.category_types.map { |ct| ["#{ct.title}(#{ct.name})", ct.id] }
+  end
+
+  def category_types
+    content.category_types.where(id: in_category_type_ids)
+  end
+
+  def in_category_type_ids
+    YAML.load(in_settings['category_type_ids'].presence || '[]')
+  end
+
+  def in_category_type_ids=(value)
+    category_type_ids = value.select(&:present?).map(&:to_i).uniq
+    ins = in_settings
+    ins['category_type_ids'] = YAML.dump(category_type_ids)
+    self.in_settings = ins
+  end
+
   private
 
   def set_default_settings
