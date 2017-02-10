5838e37e2b557f87de2b1c20aca1214fab9427ee
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2017-02-10 15:56:23 +0900

    [zomekiv3-sqr:00471] Add index page for tag node

--- app/scripts/tag/tags_script.rb
+++ app/scripts/tag/tags_script.rb
@@ -1,8 +1,7 @@
 class Tag::TagsScript < Cms::Script::Publication
   def publish
     publish_more(@node, uri: @node.public_uri, path: @node.public_path,
-                        smart_phone_path: @node.public_smart_phone_path, dependent: @node.public_uri,
-                        limit: 0)
+                        smart_phone_path: @node.public_smart_phone_path, dependent: @node.public_uri)
 
     tags = @node.content.tags
     tags = tags.where(id: params[:target_tag_id]) if params[:target_tag_id].present? 
