93685facad409e3bb38d293e5f774ede3bcb344c
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2017-02-10 14:11:13 +0900

    [zomekiv3-sqr:00470] Add list_style setting for organization group

--- app/controllers/organization/admin/piece/all_groups_controller.rb
+++ app/controllers/organization/admin/piece/all_groups_controller.rb
@@ -1,2 +1,7 @@
 class Organization::Admin::Piece::AllGroupsController < Cms::Admin::Piece::BaseController
+  private
+
+  def base_params_item_in_settings
+    [:list_style]
+  end
 end
--- app/models/organization/piece/all_group.rb
+++ app/models/organization/piece/all_group.rb
@@ -1,3 +1,19 @@
 class Organization::Piece::AllGroup < Cms::Piece
   default_scope { where(model: 'Organization::AllGroup') }
+
+  after_initialize :set_default_settings
+
+  def list_style
+    setting_value(:list_style).to_s
+  end
+
+  private
+
+  def set_default_settings
+    settings = self.in_settings
+
+    settings['list_style'] = '@name_link@' if setting_value(:list_style).nil?
+
+    self.in_settings = settings
+  end
 end
