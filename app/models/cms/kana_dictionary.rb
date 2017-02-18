ba6c6349e1d9ca77f37e9c37128a61917c08e338
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2017-02-18 14:42:30 +0900

    [zomekiv3-sqr:00489] Change dictionary path

--- app/models/cms/kana_dictionary.rb
+++ app/models/cms/kana_dictionary.rb
@@ -10,56 +10,6 @@ class Cms::KanaDictionary < ApplicationRecord
 
   before_save :convert_csv
 
-  def self.mecab_rc(_site_id=nil)
-    user_dic(_site_id) # confirm
-    return site_mecab_rc(_site_id)
-  end
-
-  def self.user_dic(_site_id=nil)
-    mecab_dir = "#{Rails.root}/config/mecab/"
-    if _site_id.blank?
-      dic = ::File.join(mecab_dir, "zomeki.dic")
-    else
-      site_dir = ::File.join(mecab_dir, "sites", format('%04d', _site_id))
-      dic = ::File.join(site_dir, "zomeki.dic")
-    end
-
-    if ::File.exists?(dic)
-      return dic
-    else
-      ::FileUtils.mkdir_p(site_dir) if !_site_id.blank? && !::File.exists?(site_dir)
-      FileUtils.cp("#{mecab_dir}zomeki.dic.original", dic)
-    end
-    return dic
-  end
-
-  def self.site_mecab_rc(_site_id=nil)
-    mecab_dir = "#{Rails.root}/config/mecab/"
-
-    if _site_id.blank?
-      rc = ::File.join(mecab_dir, "mecabrc")
-    else
-      site_dir = ::File.join("sites", format('%04d', _site_id))
-      rc = ::File.join(mecab_dir, site_dir, "mecabrc")
-    end
-
-    if ::File.exists?(rc)
-      return rc
-    else
-      ::FileUtils.mkdir_p(site_dir) if !_site_id.blank? && !::File.exists?(site_dir)
-      originalrc = "#{mecab_dir}mecabrc"
-      f = ::File.read(originalrc)
-      data = f.gsub(/zomeki\.dic/, "#{site_dir}/zomeki.dic")
-      ::File.write(rc, data)
-    end
-    return rc
-  end
-
-  def self.dic_mtime(_site_id=nil)
-    file = user_dic(_site_id)
-    ::File.mtime(file)
-  end
-
   def convert_csv
     csv = []
 
@@ -123,5 +73,49 @@ class Cms::KanaDictionary < ApplicationRecord
     FileUtils.rm(csv.path) if FileTest.exists?(csv.path)
 
     return errors.size > 0 ? errors : true
+  end
+
+  class << self
+    def mecab_rc(site_id = nil)
+      user_dic(site_id) # confirm
+      site_mecab_rc(site_id)
+    end
+
+    def user_dic(site_id = nil)
+      dic = mecab_dir(site_id).join('zomeki.dic').to_s
+      unless ::File.exists?(dic)
+        dir = ::File.dirname(dic)
+        ::FileUtils.mkdir_p(dir) unless ::Dir.exist?(dir)
+        FileUtils.cp(Rails.root.join("config/mecab/zomeki.dic.original").to_s, dic)
+      end
+      dic
+    end
+
+    def site_mecab_rc(site_id = nil)
+      rc = mecab_dir(site_id).join("mecabrc").to_s
+      unless ::File.exists?(rc)
+        dir = ::File.dirname(rc)
+        ::FileUtils.mkdir_p(dir) unless ::Dir.exist?(dir)
+        data = ::File.read(Rails.root.join("config/mecab/mecabrc").to_s).to_s
+                     .gsub(/config\//, "sites/#{format('%04d', site_id)}/config/")
+        ::File.write(rc, data)
+      end
+      rc
+    end
+
+    def dic_mtime(site_id = nil)
+      file = user_dic(site_id)
+      ::File.mtime(file)
+    end
+
+    private
+    
+    def mecab_dir(site_id = nil)
+      if site_id.blank?
+        Rails.root.join('config/mecab')
+      else
+        Rails.root.join('sites', format('%04d', site_id), 'config/mecab')
+      end
+    end
   end
 end
