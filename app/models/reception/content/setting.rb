2bed9536ad38cc1d5ac1aaf87ee7560a28fadc82
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2017-03-21 15:12:54 +0900

    [zomekiv3-spec:00123] Add upper text and lower text settings for reception mail

--- app/models/reception/content/setting.rb
+++ app/models/reception/content/setting.rb
@@ -26,6 +26,25 @@ class Reception::Content::Setting < Cms::ContentSetting
 
   validate :validate_value
 
+  def extra_values=(params)
+    ex = extra_values
+    case name
+    when 'gp_category_content_category_type_id'
+      ex[:category_type_ids] = Array(params[:category_types]).map(&:to_i)
+      ex[:visible_category_type_ids] = Array(params[:visible_category_types]).map(&:to_i)
+      ex[:default_category_type_id] = params[:default_category_type].to_i
+      ex[:default_category_id] = params[:default_category].to_i
+    when 'auto_reply'
+      ex[:applied_upper_reply_text] = params[:applied_upper_reply_text]
+      ex[:applied_lower_reply_text] = params[:applied_lower_reply_text]
+      ex[:received_upper_reply_text] = params[:received_upper_reply_text]
+      ex[:received_lower_reply_text] = params[:received_lower_reply_text]
+      ex[:canceled_upper_reply_text] = params[:canceled_upper_reply_text]
+      ex[:canceled_lower_reply_text] = params[:canceled_lower_reply_text]
+    end
+    super(ex)
+  end
+
   private
 
   def validate_value
--- lib/gp_category/model/rel/content/setting/category_type_id.rb
+++ lib/gp_category/model/rel/content/setting/category_type_id.rb
@@ -1,18 +1,6 @@
 module GpCategory::Model::Rel::Content::Setting::CategoryTypeId
   extend ActiveSupport::Concern
 
-  def extra_values=(params)
-    ex = extra_values
-    case name
-    when 'gp_category_content_category_type_id'
-      ex[:category_type_ids] = Array(params[:category_types]).map(&:to_i)
-      ex[:visible_category_type_ids] = Array(params[:visible_category_types]).map(&:to_i)
-      ex[:default_category_type_id] = params[:default_category_type].to_i
-      ex[:default_category_id] = params[:default_category].to_i
-    end
-    super(ex)
-  end
-
   def category_type_ids
     extra_values[:category_type_ids] || []
   end
