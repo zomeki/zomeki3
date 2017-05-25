985af4179838c4f1aa0c149a82627eb28e069c7f
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2017-05-25 11:29:08 +0900

    [zomekiv3-sqr:00671] Fix marker image for docs with template

--- app/helpers/map/map_helper.rb
+++ app/helpers/map/map_helper.rb
@@ -20,25 +20,10 @@ module Map::MapHelper
   end
 
   def marker_image(marker)
-    unless (doc = marker.doc)
-      file = marker.files.first
-      return '' unless file.parent.content.public_node
-      return image_tag("#{file.parent.content.public_node.public_uri}#{file.parent.name}/file_contents/#{url_encode file.name}")
-    else
-      return '' unless doc.content.public_node
-    end
-
-    image_file = doc.image_files.detect{|f| f.name == doc.list_image } || doc.image_files.first if doc.list_image.present?
-
-    if image_file
-      image_tag("#{doc.content.public_node.public_uri}#{doc.name}/file_contents/#{url_encode image_file.name}")
-    else
-      unless (img_tags = Nokogiri::HTML.parse(doc.body).css('img[src^="file_contents/"]')).empty?
-        filename = File.basename(img_tags.first.attributes['src'].value)
-        image_tag("#{doc.content.public_node.public_uri}#{doc.name}/file_contents/#{url_encode filename}")
-      else
-        ''
-      end
+    if (doc = marker.doc) && doc.content.public_node
+      doc_image_tag(doc)
+    elsif (file = marker.files.first) && file.parent.content.public_node
+      image_tag("#{file.parent.content.public_node.public_uri}#{file.parent.name}/file_contents/#{url_encode file.name}")
     end
   end
 
