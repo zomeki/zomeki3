2abc9d0d8aa6dfb227b77957ed3cfb466e06020b
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2017-06-06 11:28:40 +0900

    [zomekiv3-sqr:00675] Refactor map helpers

--- app/helpers/map/map_helper.rb
+++ app/helpers/map/map_helper.rb
@@ -1,24 +1,4 @@
 module Map::MapHelper
-  def default_lat_lng
-    if @content.default_map_position.blank?
-      if @markers.empty?
-        [0, 0]
-      else
-        [@markers.first.latitude, @markers.first.longitude]
-      end
-    else
-      @content.default_map_position
-    end
-  end
-
-  def default_latitude
-    default_lat_lng.first
-  end
-
-  def default_longitude
-    default_lat_lng.last
-  end
-
   def marker_image(marker)
     if (doc = marker.doc) && doc.content.public_node
       GpArticle::Public::DocFormatService.new(doc).format("@image_tag@")
