ef5c56e777b91a80d6c799a94b3fa5a45f831c18
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2017-06-16 10:02:37 +0900

    [zomekiv3-sqr:00714] Add publish script for ad_banner

--- app/scripts/ad_banner/banners_script.rb
+++ app/scripts/ad_banner/banners_script.rb
@@ -1,4 +1,13 @@
 class AdBanner::BannersScript < Cms::Script::Publication
   def publish
+    banners = @node.content.banners
+
+    ::Script.total banners.size
+
+    banners.each do |banner|
+      ::Script.progress(banner) do
+        banner.publish_or_close_images
+      end
+    end
   end
 end
