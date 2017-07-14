727441aceb887162ce4b3e1930c05d56847c3cc1
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2017-07-14 17:18:50 +0900

    [zomekiv3-sqr:00742] Fix link check for relative path

--- app/models/cms/link.rb
+++ app/models/cms/link.rb
@@ -11,12 +11,10 @@ class Cms::Link < ApplicationRecord
     uri = Addressable::URI.parse(url)
     if uri.absolute?
       uri.to_s
-    else
-      if uri.path =~ /^\//
-        "#{site.full_uri.sub(/\/$/, '')}#{uri.path}"
-      else
-        nil
-      end
+    elsif uri.path =~ /^\//
+      Addressable::URI.join(site.full_uri, uri.path).to_s
+    elsif linkable.respond_to?(:public_uri) && (public_uri = linkable.public_uri).present?
+      Addressable::URI.join(site.full_uri, public_uri, url).to_s
     end
   rescue => e
     error_log e
