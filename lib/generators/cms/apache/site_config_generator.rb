bda248db08c4da938c515c227c8f06f9585b16eb
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2017-09-28 11:56:24 +0900

    [zomekiv3-sqr:00788] Remove update_server_configs task from cron, touch reload_servers.txt if config files are changed

--- lib/generators/cms/apache/site_config_generator.rb
+++ lib/generators/cms/apache/site_config_generator.rb
@@ -13,7 +13,7 @@ module Cms
           if site.admin_full_uri.present?
             template 'virtual_hosts/admin_virtual_host.conf.erb', site.apache_admin_config_path
           else
-            remove_file site.apache_admin_config_path
+            remove_file site.apache_admin_config_path if File.exist?(site.apache_admin_config_path)
           end
         end
       end
@@ -21,8 +21,11 @@ module Cms
       def delete_configs
         deleted_site_ids = conf_site_ids - load_sites.pluck(:id)
         deleted_site_ids.each do |site_id|
-          remove_file "config/apache/virtual_hosts/site_#{'%04d' % site_id}.conf"
-          remove_file "config/apache/admin_virtual_hosts/site_#{'%04d' % site_id}.conf"
+          files = ["config/apache/virtual_hosts/site_#{'%04d' % site_id}.conf",
+                   "config/apache/admin_virtual_hosts/site_#{'%04d' % site_id}.conf"]
+          files.each do |file|
+            remove_file file if ::File.exist?(file)
+          end
         end
       end
 
