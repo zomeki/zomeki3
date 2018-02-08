caa785fe0ddd131b612624e3c5907578c34394aa
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2018-02-08 18:00:22 +0900

    [zomekiv3-spec:00212] Add search for ad_banner clicks Conflicts: 	app/views/ad_banner/admin/clicks/index.html.erb

--- app/controllers/ad_banner/admin/clicks_controller.rb
+++ app/controllers/ad_banner/admin/clicks_controller.rb
@@ -4,12 +4,16 @@ class AdBanner::Admin::ClicksController < Cms::Controller::Admin::Base
   def pre_dispatch
     @content = AdBanner::Content::Banner.find(params[:content])
     return error_auth unless Core.user.has_priv?(:read, item: @content.concept)
+    return redirect_to url_for(action: :index) if params[:reset]
 
     @banner = @content.banners.find(params[:banner_id])
   end
 
   def index
-    @items = @banner.clicks.paginate(page: params[:page], per_page: 50)
+    @items = AdBanner::ClicksFinder.new(@banner.clicks)
+                                   .search(params[:criteria])
+                                   .paginate(page: params[:page], per_page: 50)
+
     _index @items
   end
 end
