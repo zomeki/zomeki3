d455b19318887235cfd81c2156b8193c91789610
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2018-02-23 18:47:30 +0900

    [zomekiv3-spec:00207] Concatenate question title for filename of attachment

--- app/services/survey/attachment_compress_service.rb
+++ app/services/survey/attachment_compress_service.rb
@@ -8,7 +8,7 @@ class Survey::AttachmentCompressService < ApplicationService
     steam = Zip::OutputStream.write_buffer(StringIO.new('')) do |out|
       @answers.preload(:attachment).find_each(batch_size: 10) do |answer|
         next unless attachment = answer.attachment
-        out.put_next_entry("#{answer.form_answer_id}_#{attachment.name}")
+        out.put_next_entry("#{answer.form_answer_id}_#{answer.question.title}_#{attachment.name}")
         out.write(attachment.data)
       end
     end
