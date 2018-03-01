059496623f7bf9b1868b6a35e75abed1a5960736
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2018-03-01 15:53:12 +0900

    [zomekiv3-sqr:00861] Delete related data before site deletion

---
+++ app/services/cms/site_scan_service.rb
@@ -0,0 +1,24 @@
+class Cms::SiteScanService < ApplicationService
+  def initialize(site)
+    @site = site
+  end
+
+  def scan
+    models = load_models
+    models.each_with_object({}) do |model, hash|
+      hash[model] = model.unscoped
+                         .in_site(@site)
+                         .order(model.primary_key)
+                         .pluck(model.primary_key)
+    end
+  end
+
+  private
+
+  def load_models
+    Rails.application.eager_load!
+    klasses = ActiveRecord::Base.descendants.select { |klass| klass.respond_to?(:in_site) }
+    klasses.select! { |klass| ((klass.ancestors - [klass]) & klasses).empty? }
+    klasses.sort_by(&:name)
+  end
+end
