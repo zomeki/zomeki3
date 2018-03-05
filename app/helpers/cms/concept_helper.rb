1bd8c962f31c6306c4282c16e7b8c1c551a0a0ad
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2018-03-05 15:33:51 +0900

    [zomekiv3-sqr:00845] Don't close concept tree after page transition

--- app/helpers/cms/concept_helper.rb
+++ app/helpers/cms/concept_helper.rb
@@ -15,7 +15,13 @@ module Cms::ConceptHelper
         html = ''
         html << link_to(icon_mark, "#", id: "naviConceptIcon#{concept.id}", class: icon_cls.join(' '))
         html << " "
-        html << link_to(concept.name, cms_navi_concept_path(concept.id), id: "naviConceptItem#{concept.id}", class: item_cls.join(' '))
+
+        url = if request.fullpath.split('/')[2].in?(%w(sys cms))
+                { action: :index, concept: concept.id }
+              else
+                cms_navi_concept_path(concept.id)
+              end
+        html << link_to(concept.name, url, id: "naviConceptItem#{concept.id}", class: item_cls.join(' '))
 
         if children.size > 0
           html << content_tag(:ul, id: "naviConceptList#{concept.id}") do
