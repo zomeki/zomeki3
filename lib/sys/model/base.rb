e769c7307d90280cafd7916e1e0ad967b9402f20
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2018-03-07 10:01:03 +0900

    [zomekiv3-sqr:00869] Refactor integer validator

--- lib/sys/model/base.rb
+++ lib/sys/model/base.rb
@@ -1,9 +1,9 @@
 module Sys::Model::Base
   extend ActiveSupport::Concern
   include Sys::Model::Scope
-  include Sys::Model::Validation
 
   included do
     self.table_name = self.to_s.underscore.gsub('/', '_').downcase.pluralize
+    validates_with IntegerValidator
   end
 end
