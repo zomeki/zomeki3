f0f36c65cefe0a63d92a6ebcf94b25300a861bbb
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2018-04-25 10:11:13 +0900

    [zomekiv3-spec:00243] Add replace feature

--- app/controllers/cms/admin/tool/search_controller.rb
+++ app/controllers/cms/admin/tool/search_controller.rb
@@ -4,172 +4,64 @@ class Cms::Admin::Tool::SearchController < Cms::Controller::Admin::Base
   def pre_dispatch
     return error_auth unless Core.user.has_auth?(:creator)
     return redirect_to(action: :index) if params[:reset]
+
+    if params[:target].nil?
+      params[:target] = ['gp_article', 'node_page', 'piece', 'layout', 'data_text', 'data_file']
+    end
   end
 
   def index
     @results = []
-    return true if params[:do] != 'search'
-    return true if params[:keyword].blank?
+    return if params[:keyword].blank?
 
-    concepts = load_target_concepts
-
-    if params[:target] && params[:target][:node_page] && Core.user.has_auth?(:designer)
-      @results << {
-        title: "固定ページ",
-        items: search_cms_nodes(Core.site, concepts, params)
-      }
-    end
-
-    if params[:target] && params[:target][:gp_article]
-      contents = Cms::Content.where(site_id: Core.site.id, model: "GpArticle::Doc", concept_id: concepts.map(&:id))
-                             .order(:id)
-      contents.each do |content|
-        @results << {
-          title: "記事：#{content.name}",
-          items: search_gp_article_docs(content, params)
-        }
+    if Core.user.has_auth?(:manager) && params[:replace]
+      if params[:replace_word].present?
+        Cms::SiteSearchService.new(Core.site, Core.user, Core.concept).replace(params)
+        flash.now[:notice] = '置換処理が完了しました。置換内容を公開画面へ反映するには再構築を実行してください。'
+      else
+        flash.now[:notice] = '置換する文字列を指定してください。'
       end
     end
 
-    if params[:target] && params[:target][:piece] && Core.user.has_auth?(:designer)
-      @results << {
-        title: "ピース",
-        items: search_cms_pieces(Core.site, concepts, params)
-      }
+    @results = Cms::SiteSearchService.new(Core.site, Core.user, Core.concept).search(params)
+    @results.each do |result|
+      result[:title] = display_title(result[:model], result[:content])
+      result[:items] = display_items(result[:items], result[:content])
     end
 
-    if params[:target] && params[:target][:layout] && Core.user.has_auth?(:designer)
-      @results << {
-        title: "レイアウト",
-        items: search_cms_layouts(Core.site, concepts, params)
-      }
-    end
-
-    if params[:target] && params[:target][:data_text]
-      @results << {
-        title: "テキスト",
-        items: search_cms_data_texts(Core.site, concepts, params)
-      }
-    end
-
-    if params[:target] && params[:target][:data_file]
-      @results << {
-        title: "ファイル",
-        items: search_cms_data_files(Core.site, concepts, params)
-      }
-    end
+    @count = @results.map { |result| result[:count] }.sum
   end
 
   private
 
-  def load_target_concepts
-    if params[:target_concept] == 'current'
-      [Core.concept]
-    elsif Core.user.has_auth?(:manager)
-      Core.site.concepts
-    else
-      Core.site.concepts.readable_for(Core.user).to_tree.flat_map(&:descendants)
+  def display_title(model, content)
+    case model.to_s
+    when 'GpArticle::Doc'
+      "記事：#{content.name}"
+    when 'Cms::Node'
+      '固定ページ'
+    when 'Cms::Piece'
+      'ピース'
+    when 'Cms::Layout'
+      'レイアウト'
+    when 'Cms::DataText'
+      'テキスト'
+    when 'Cms::DataFile'
+      'ファイル'
     end
   end
 
-  def search_cms_nodes(site, concepts, criteria)
-    items = Cms::Node.where(site_id: site.id, model: "Cms::Page", concept_id: concepts.map(&:id))
-                     .search_with_text(:title, :body, :mobile_title, :mobile_body, criteria[:keyword])
-                     .order(:id)
-    items.map { |c|
+  def display_items(items, content)
+    items.map do |item|
       {
-        id: c.id,
-        title: c.title,
-        state: c.state,
-        state_text: c.state_text,
-        concept: c.concept,
-        admin_uri: c.admin_uri,
-        public_uri: c.public_full_uri
+        id: item.id,
+        title: item.title,
+        state: item.state,
+        state_text: item.state_text,
+        concept: content ? content.concept : item.concept,
+        admin_uri: item.admin_uri,
+        public_uri: item.respond_to?(:public_full_uri) ? item.public_full_uri : nil
       }
-    }
-  end
-
-  def search_gp_article_docs(content, criteria)
-    publics = GpArticle::Doc.where(state: 'public').where(content_id: content.id)
-    non_publics = GpArticle::Doc.where.not(state: 'public').where(content_id: content.id)
-    items = [publics,
-             non_publics.editable,
-             non_publics.creator_or_approvables(Core.user)].reduce(:union)
-    items = items.search_with_text(:title, :subtitle, :summary, :body, :mobile_title, :mobile_body, criteria[:keyword])
-                 .order(:id)
-    items.map { |c|
-      {
-        id: c.id,
-        title: c.title,
-        state: c.state,
-        state_text: c.state_text,
-        concept: content.concept,
-        admin_uri: gp_article_doc_path(c.content, c.id),
-        public_uri: c.public_full_uri
-      }
-    }
-  end
-
-  def search_cms_pieces(site, concepts, criteria)
-    items = Cms::Piece.where(site_id: site.id, concept_id: concepts.map(&:id))
-                      .search_with_text(:name, :title, :view_title, :head, :body, criteria[:keyword])
-                      .order(:id)
-    items.map { |c|
-      {
-        id: c.id,
-        title: c.title,
-        state: c.state,
-        state_text: c.state_text,
-        concept: c.concept,
-        admin_uri: c.admin_uri
-      }
-    }
-  end
-
-  def search_cms_layouts(site, concepts, criteria)
-    items = Cms::Layout.where(site_id: site.id, concept_id: concepts.map(&:id))
-                       .search_with_text(:name, :title, :head, :body, :mobile_head, :mobile_body, criteria[:keyword])
-                       .order(:id)
-    items.map { |c|
-      {
-        id: c.id,
-        title: c.title,
-        concept: c.concept,
-        admin_uri: cms_layout_path(c.concept_id, c.id)
-      }
-    }
-  end
-
-  def search_cms_data_texts(site, concepts, criteria)
-    items = Cms::DataText.where(site_id: site.id, concept_id: concepts.map(&:id))
-                         .search_with_text(:name, :title, :body, criteria[:keyword])
-                         .order(:id)
-    items.map { |c|
-      {
-        id: c.id,
-        title: c.title,
-        state: c.state,
-        state_text: c.state_text,
-        concept: c.concept,
-        admin_uri: cms_data_text_path(c.concept_id, c.id)
-      }
-    }
-  end
-
-  def search_cms_data_files(site, concepts, criteria)
-    items = Cms::DataFile.where(site_id: site.id, concept_id: concepts.map(&:id))
-                         .search_with_text(:name, :title, criteria[:keyword])
-                         .order(:id)
-    items.map { |c|
-      {
-        id: c.id,
-        title: c.title,
-        state: c.state,
-        state_text: c.state_text,
-        concept: c.concept,
-        admin_uri: cms_data_file_path(c.concept_id, c.id),
-        public_uri: c.public_full_uri
-      }
-    }
+    end
   end
 end
---
+++ app/services/cms/site_search_service.rb
@@ -0,0 +1,149 @@
+class Cms::SiteSearchService < ApplicationService
+  def initialize(site, user, current_concept)
+    @site = site
+    @user = user
+    @current_concept = current_concept
+
+    @columns = {
+      Cms::Node => [:title, :body, :mobile_title, :mobile_body],
+      Cms::Piece => [:name, :title, :view_title, :body],
+      Cms::Layout => [:name, :title, :head, :body, :mobile_head, :mobile_body, :smart_phone_head, :smart_phone_body],
+      Cms::DataText => [:name, :title, :body],
+      Cms::DataFile => [:name, :title, :alt_text],
+      GpArticle::Doc => [:title, :subtitle, :summary, :body, :body_more, :mobile_title, :mobile_body,
+                         :event_note, :meta_description, :meta_keywords, :remark]
+    }
+  end
+
+  def search(criteria)
+    concepts = load_target_concepts(criteria)
+
+    results = []
+
+    if criteria[:target].include?('gp_article')
+      contents = load_gp_article_contents(concepts)
+      contents.each do |content|
+        items = search_gp_article_docs(content, criteria)
+        results << { model: GpArticle::Doc,
+                     content: content,
+                     items: items,
+                     count: items.size }
+      end
+    end
+
+    if criteria[:target].include?('node_page') && @user.has_auth?(:designer)
+      items = search_cms_nodes(concepts, criteria)
+      results << { model: Cms::Node,
+                   items: items,
+                   count: items.size }
+    end
+
+    if criteria[:target].include?('piece') && @user.has_auth?(:designer)
+      items = search_cms_pieces(concepts, criteria)
+      results << { model: Cms::Piece,
+                   items: items,
+                   count: items.size }
+    end
+
+    if criteria[:target].include?('layout') && @user.has_auth?(:designer)
+      items = search_cms_layouts(concepts, criteria)
+      results << { model: Cms::Layout,
+                   items: items,
+                   count: items.size }
+    end
+
+    if criteria[:target].include?('data_text')
+      items = search_cms_data_texts(concepts, criteria)
+      results << { model: Cms::DataText,
+                   items: items,
+                   count: items.size }
+    end
+
+    if criteria[:target].include?('data_file')
+      items = search_cms_data_files(concepts, criteria)
+      results << { model: Cms::DataFile,
+                   items: items,
+                   count: items.size }
+    end
+
+    results
+  end
+
+  def replace(criteria)
+    results = search(criteria)
+
+    results.each do |result|
+      model = result[:model]
+      ids = result[:items].map(&:id)
+
+      @columns[model].each do |column|
+        model.where(id: ids).replace_for_all(column, criteria[:keyword], criteria[:replace_word])
+      end
+    end
+  end
+
+  private
+
+  def load_target_concepts(criteria)
+    if criteria[:target_concept] == 'current'
+      [@current_concept]
+    elsif @user.has_auth?(:manager)
+      @site.concepts
+    else
+      @site.concepts.readable_for(@user).to_tree.flat_map(&:descendants)
+    end
+  end
+
+  def load_gp_article_contents(concepts)
+    GpArticle::Content::Doc.in_site(@site)
+                           .where(concept_id: concepts.map(&:id))
+                           .order(:id)
+  end
+
+  def search_gp_article_docs(content, criteria)
+    docs = GpArticle::Doc.where(content_id: content.id)
+    publics = docs.where(state: 'public')
+    non_publics = docs.where.not(state: 'public')
+
+    items = [publics,
+             non_publics.editable,
+             non_publics.creator_or_approvables(@user)].reduce(:union)
+    items.search_with_text(@columns[GpArticle::Doc], criteria[:keyword])
+         .order(:id)
+  end
+
+  def search_cms_nodes(concepts, criteria)
+    Cms::Node.in_site(@site)
+             .where(model: "Cms::Page", concept_id: concepts.map(&:id))
+             .search_with_text(@columns[Cms::Node], criteria[:keyword])
+             .order(:id)
+  end
+
+  def search_cms_pieces(concepts, criteria)
+    Cms::Piece.in_site(@site)
+              .where(concept_id: concepts.map(&:id))
+              .search_with_text(@columns[Cms::Piece], criteria[:keyword])
+              .order(:id)
+  end
+
+  def search_cms_layouts(concepts, criteria)
+    Cms::Layout.in_site(@site)
+               .where(concept_id: concepts.map(&:id))
+               .search_with_text(@columns[Cms::Layout], criteria[:keyword])
+               .order(:id)
+  end
+
+  def search_cms_data_texts(concepts, criteria)
+    Cms::DataText.in_site(@site)
+                 .where(concept_id: concepts.map(&:id))
+                 .search_with_text(@columns[Cms::DataText], criteria[:keyword])
+                 .order(:id)
+  end
+
+  def search_cms_data_files(concepts, criteria)
+    Cms::DataFile.in_site(@site)
+                 .where(concept_id: concepts.map(&:id))
+                 .search_with_text(@columns[Cms::DataFile], criteria[:keyword])
+                 .order(:id)
+  end
+end
