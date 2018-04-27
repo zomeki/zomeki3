5b06315f87714c01f6f4f2142e9d1948449fa1f1
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2018-04-27 15:41:05 +0900

    [zomekiv3-sqr:00942] Cut off unnecessary page rendering

--- app/scripts/gnav/menu_items_script.rb
+++ app/scripts/gnav/menu_items_script.rb
@@ -5,7 +5,7 @@ class Gnav::MenuItemsScript < PublicationScript
                         smart_phone_path: @node.public_smart_phone_path)
 
     @node.content.public_menu_items.each do |menu_item|
-      publish_more(menu_item, uri: menu_item.public_uri,
+      publish_page(menu_item, uri: menu_item.public_uri,
                               path: menu_item.public_path,
                               smart_phone_path: menu_item.public_smart_phone_path)
     end
