f8af51677e09ee88e6f218c300caf52e61b56220
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2018-07-17 09:57:39 +0900

    [zomekiv3-sqr:01065] Refactor javascript

--- public/_common/js/cms_admin/form.js
+++ public/_common/js/cms_admin/form.js
@@ -1,227 +1,237 @@
 // toggle open
-jQuery.extend(jQuery.fn, {
-  toggleOpen: function(target, openLabel, closeLabel, speed) {
-    if (openLabel == undefined)  openLabel  = "開く▼";
-    if (closeLabel == undefined) closeLabel = "閉じる▲";
-    if (speed == undefined) speed = 200;
+(function($) {
+  $.extend($.fn, {
+    toggleOpen: function(target, openLabel, closeLabel, speed) {
+      if (openLabel == undefined) { openLabel  = "開く▼"; }
+      if (closeLabel == undefined) { closeLabel = "閉じる▲"; }
+      if (speed == undefined) { speed = 200; }
 
-    if (jQuery(target).css('display') == 'none') {
-      jQuery(this).html(closeLabel);
-    } else {
-      jQuery(this).html(openLabel);
-    }
+      if ($(target).css('display') == 'none') {
+        $(this).html(closeLabel);
+      } else {
+        $(this).html(openLabel);
+      }
 
-    if (speed == 0) {
-      jQuery(target).toggle(speed);
-    } else {
-      jQuery(target).slideToggle(speed);
+      if (speed == 0) {
+        $(target).toggle(speed);
+      } else {
+        $(target).slideToggle(speed);
+      }
     }
-  }
-});
+  });
+})(jQuery);
 
 // add fields
-jQuery.extend(jQuery.fn, {
-  addFields: function(options) {
-    var button = jQuery(this);
-    var setting = jQuery.extend({
-      container: '.container',
-      fields: '.fields',
-      startIndex: 0,
-      indexName: '_attributes',
-      replaceTags: {
-        input: ['id', 'name'],
-        textarea: ['id', 'name'],
-        select: ['id', 'name'],
-        label: ['for']
-      },
-      extraReplaceTags: undefined,
-      beforeAdd: undefined,
-      afterAdd: undefined
-    }, options);
+(function($) {
+  $.extend($.fn, {
+    addFields: function(options) {
+      var $button = $(this);
+      var setting = $.extend({
+        container: '.container',
+        fields: '.fields',
+        startIndex: 0,
+        indexName: '_attributes',
+        replaceTags: {
+          input: ['id', 'name'],
+          textarea: ['id', 'name'],
+          select: ['id', 'name'],
+          label: ['for']
+        },
+        extraReplaceTags: null,
+        beforeAdd: null,
+        afterAdd: null
+      }, options);
 
-    if (Array.isArray(setting.indexName)) {
-      setting.indexNames = setting.indexName;
-    } else {
-      setting.indexNames = [setting.indexName];
-    }
-
-    if (setting.extraReplaceTags) {
-      jQuery.extend(setting.replaceTags, setting.extraReplaceTags);
-    };
-
-    button.on('click', function(e) {
-      e.preventDefault();
-
-      var clone = jQuery(setting.container).find(setting.fields + ':last').clone(true);
-      clearInputValue(clone);
-      replaceIndex(clone);
-
-      if (setting.beforeAdd) {
-        setting.beforeAdd(clone);
+      if (Array.isArray(setting.indexName)) {
+        setting.indexNames = setting.indexName;
+      } else {
+        setting.indexNames = [setting.indexName];
       }
 
-      jQuery(setting.container).append(clone);
+      if (setting.extraReplaceTags) {
+        $.extend(setting.replaceTags, setting.extraReplaceTags);
+      };
 
-      if (setting.afterAdd) {
-        setting.afterAdd(clone);
-      }
-    });
+      $button.on('click', function(e) {
+        e.preventDefault();
 
-    var clearInputValue = function(clone) {
-      clone.find('textarea').text('');
-      clone.find('input[type="text"]').val('').removeAttr('value');
-      clone.find('input[type="checkbox"], input[type="radio"]').prop('checked', false);
-      clone.find('option').prop('selected', false);
+        var $clone = $(setting.container).find(setting.fields + ':last').clone(true);
+        clearInputValue($clone);
+        replaceIndex($clone);
 
-      // check first radio button
-      var names = findRadioButtonNames(clone);
-      names.forEach(function(name) {
-        clone.find('input[type="radio"][name="' + name +'"]').first().prop('checked', true);
+        if (setting.beforeAdd) {
+          setting.beforeAdd($clone);
+        }
+
+        $(setting.container).append($clone);
+
+        if (setting.afterAdd) {
+          setting.afterAdd($clone);
+        }
       });
 
-      // remove hidden field for nested form
-      var regexps = makeRegexpsForNestedFormID();
-      clone.find('input[type="hidden"]').each(function() {
-        var elem = $(this);
-        var id = elem.attr('id');
-        regexps.forEach(function(regexp) {
-          if (id =~ regexp) {
-            elem.remove();
-            return;
-          }
+      function clearInputValue($clone) {
+        $clone.find('textarea').text('');
+        $clone.find('input[type="text"]').val('').removeAttr('value');
+        $clone.find('input[type="checkbox"], input[type="radio"]').prop('checked', false);
+        $clone.find('option').prop('selected', false);
+
+        // check first radio button
+        var names = findRadioButtonNames($clone);
+        names.forEach(function(name) {
+          $clone.find('input[type="radio"][name="' + name +'"]').first().prop('checked', true);
         });
-      });
-    };
 
-    var findRadioButtonNames = function(clone) {
-      var names = [];
-      clone.find('input[type="radio"]').each(function() {
-        var name = $(this).attr('name');
-        if (names.indexOf(name) == -1) { names.push(name); }
-      });
-      return names;
-    };
-
-    var makeRegexpsForNestedFormID = function() {
-      return setting.indexNames.map(function(name) {
-        new RegExp(name + '_\\d+_id');
-      });
-    };
-
-    var replaceIndex = function(clone) {
-      var nextID = jQuery(setting.container).find(setting.fields).length + setting.startIndex;
-      var regs = makeRegexpsForReplace();
-
-      for (tag in setting.replaceTags) {
-        var attrs = setting.replaceTags[tag];
-        clone.find(tag).each(function() {
-          var elem = jQuery(this);
-          attrs.forEach(function(attr) {
-            var value = elem.attr(attr);
-            if (value) {
-              regs.forEach(function(reg) {
-                value = value.replace(reg, '$1' + nextID);
-              });
-              elem.attr(attr, value);
+        // remove hidden field for nested form
+        var regexps = makeRegexpsForNestedFormID();
+        $clone.find('input[type="hidden"]').each(function() {
+          var $elem = $(this);
+          var id = $elem.attr('id');
+          regexps.forEach(function(regexp) {
+            if (id =~ regexp) {
+              $elem.remove();
+              return;
             }
           });
         });
       }
-    };
 
-    var makeRegexpsForReplace = function() {
-      var regexps = [];
-      setting.indexNames.forEach(function(name) {
-        regexps.push(new RegExp('(' + name + '_)\\d+'));
-        regexps.push(new RegExp('(' + name + '\\[)\\d+'));
-        regexps.push(new RegExp('(' + name + '\\]\\[)\\d+'));
-      });
-      return regexps;
-    };
-  }
-});
+      function findRadioButtonNames($clone) {
+        var names = [];
+        $clone.find('input[type="radio"]').each(function() {
+          var name = $(this).attr('name');
+          if (names.indexOf(name) == -1) { names.push(name); }
+        });
+        return names;
+      }
+
+      function makeRegexpsForNestedFormID() {
+        return setting.indexNames.map(function(name) {
+          new RegExp(name + '_\\d+_id');
+        });
+      }
+
+      function replaceIndex($clone) {
+        var nextID = $(setting.container).find(setting.fields).length + setting.startIndex;
+        var regs = makeRegexpsForReplace();
+
+        for (tag in setting.replaceTags) {
+          var attrs = setting.replaceTags[tag];
+          $clone.find(tag).each(function() {
+            var $elem = $(this);
+            attrs.forEach(function(attr) {
+              var value = $elem.attr(attr);
+              if (value) {
+                regs.forEach(function(reg) {
+                  value = value.replace(reg, '$1' + nextID);
+                });
+                $elem.attr(attr, value);
+              }
+            });
+          });
+        }
+      }
+
+      function makeRegexpsForReplace() {
+        var regexps = [];
+        setting.indexNames.forEach(function(name) {
+          regexps.push(new RegExp('(' + name + '_)\\d+'));
+          regexps.push(new RegExp('(' + name + '\\[)\\d+'));
+          regexps.push(new RegExp('(' + name + '\\]\\[)\\d+'));
+        });
+        return regexps;
+      }
+    }
+  });
+})(jQuery);
 
 // simple multi select
-jQuery.extend(jQuery.fn, {
-  simpleMultiSelect: function(options) {
-   var form = $(this);
-    var setting = jQuery.extend({
-      source: '.source',
-      destination: '.destination',
-      add: '.add',
-      remove: '.remove',
-      beforeAdd: undefined,
-      afterAdd: undefined,
-      afterRemove: undefined,
-      afterRemove: undefined,
-    }, options);
+(function($) {
+  $.extend($.fn, {
+    simpleMultiSelect: function(options) {
+     var $form = $(this);
+      var setting = $.extend({
+        source: '.source',
+        destination: '.destination',
+        add: '.add',
+        remove: '.remove',
+        beforeAdd: undefined,
+        afterAdd: undefined,
+        afterRemove: undefined,
+        afterRemove: undefined,
+      }, options);
 
-    form.on('submit', function() {
-      form.find(setting.source).find('option').prop('selected', false);
-      form.find(setting.destination).find('option').prop('selected', true);
-    });
-    form.find(setting.add).on('click', function() {
-      addSelectedOptions();
-    });
-    form.find(setting.source).on('dblclick', function() {
-      addSelectedOptions();
-    });
-    form.find(setting.remove).on('click', function() {
-      removeSelectedOptions();
-    });
-    form.find(setting.destination).on('dblclick', function() {
-      removeSelectedOptions();
-    });
-
-    var addSelectedOptions = function() {
-      var from = form.find(setting.source);
-      var to = form.find(setting.destination);
-      from.find('option:selected').each(function() {
-        var option = $(this);
-        if (to.find('option[value="' + option.val() + '"]').length == 0) {
-          option.prop('selected', false);
-
-          if (setting.beforeAdd && setting.beforeAdd(option) == false) { return; }
-          to.append(option);
-          if (setting.afterAdd && setting.afterAdd(option) == false) { return; }
-        }
+      $form.on('submit', function() {
+        $form.find(setting.source).find('option').prop('selected', false);
+        $form.find(setting.destination).find('option').prop('selected', true);
       });
-    };
-
-    var removeSelectedOptions = function() {
-      var from = form.find(setting.destination);
-      var to = form.find(setting.source);
-      from.find('option:selected').each(function() {
-        var option = $(this);
-        if (to.find('option[value="' + option.val() + '"]').length == 0) {
-          option.prop('selected', false);
-
-          if (setting.beforeRemove && setting.beforeRemove(option) == false) { return; }
-          to.append(option);
-          if (setting.afterRemove && setting.afterRemove(option) == false) { return; }
-        }
+      $form.find(setting.add).on('click', function() {
+        addSelectedOptions();
       });
-    };
-  }
-});
+      $form.find(setting.source).on('dblclick', function() {
+        addSelectedOptions();
+      });
+      $form.find(setting.remove).on('click', function() {
+        removeSelectedOptions();
+      });
+      $form.find(setting.destination).on('dblclick', function() {
+        removeSelectedOptions();
+      });
 
-jQuery(function() {
-  jQuery('form.new_item, form.edit_item').on('keypress', function(e) {
-    if (e.target.type !== 'textarea' && e.which === 13) return false;
-  });
+      function addSelectedOptions() {
+        var $from = $form.find(setting.source);
+        var $to = $form.find(setting.destination);
+        $from.find('option:selected').each(function() {
+          var option = $(this);
+          if ($to.find('option[value="' + option.val() + '"]').length == 0) {
+            option.prop('selected', false);
+  
+            if (setting.beforeAdd && setting.beforeAdd(option) == false) { return; }
+            $to.append(option);
+            if (setting.afterAdd && setting.afterAdd(option) == false) { return; }
+          }
+        });
+      }
 
-  function beforeunload() {
-    jQuery(window).on('beforeunload', function() {
-      return 'このページを離れるとフォームに入力したデータが失われます。本当に移動してよろしいですか？';
-    });
-  }
-  jQuery('form.new_item, form.edit_item').on('change', beforeunload);
-  if (typeof CKEDITOR != 'undefined') {
-    for (var i in CKEDITOR.instances) {
-      CKEDITOR.instances[i].on('change', beforeunload);
+      function removeSelectedOptions() {
+        var $from = $form.find(setting.destination);
+        var $to = $form.find(setting.source);
+        $from.find('option:selected').each(function() {
+          var option = $(this);
+          if ($to.find('option[value="' + option.val() + '"]').length == 0) {
+            option.prop('selected', false);
+
+            if (setting.beforeRemove && setting.beforeRemove(option) == false) { return; }
+            $to.append(option);
+            if (setting.afterRemove && setting.afterRemove(option) == false) { return; }
+          }
+        });
+      }
     }
-  }
-  jQuery('form.new_item input[type=submit], form.edit_item input[type=submit]').on('click', function(e) {
-    jQuery(window).off('beforeunload');
   });
-});
+})(jQuery);
+
+(function($) {
+  $(function() {
+    // ignore enter key
+    $('form.new_item, form.edit_item').on('keypress', function(e) {
+      if (e.target.type !== 'textarea' && e.which === 13) { return false; }
+    });
+
+    // show message before move to another page
+    function beforeunload() {
+      $(window).on('beforeunload', function() {
+        return 'このページを離れるとフォームに入力したデータが失われます。本当に移動してよろしいですか？';
+      });
+    }
+    $('form.new_item, form.edit_item').on('change', beforeunload);
+    if (typeof CKEDITOR != 'undefined') {
+      for (var i in CKEDITOR.instances) {
+        CKEDITOR.instances[i].on('change', beforeunload);
+      }
+    }
+    $('form.new_item input[type=submit], form.edit_item input[type=submit]').on('click', function(e) {
+      $(window).off('beforeunload');
+    });
+  });
+})(jQuery);
--- public/_common/js/cms_admin/navi.js
+++ public/_common/js/cms_admin/navi.js
@@ -1,28 +1,30 @@
-$(function() {
-  $('#currentNaviSite').on('click', function() {
-    $('#naviConcepts').hide();
+(function($) {
+  $(function() {
+    $('#currentNaviSite').on('click', function() {
+      $('#naviConcepts').hide();
 
-    var view = $('#naviSites');
-    if (view.attr('id')) {
-      view.toggle();
-    } else {
-      if (this.loading) return false;
-      this.loading = true;
-      
-      var uri = $(this).attr('href');
-      jQuery.ajax({
-        url: uri,
-        success: function(data, dataType) {
-          $('#content').prepend(data);
-        }
-      });
-    }
-    return false;
-  });
+      var $view = $('#naviSites');
+      if ($view.attr('id')) {
+        $view.toggle();
+      } else {
+        if (this.loading) return false;
+        this.loading = true;
 
-  $('#currentNaviConcept').on('click', function() {
-    $('#naviSites').hide();
-    $('#naviConcepts').toggle();
-    $.cookie("naviConceptsVisible", $('#naviConcepts').is(':visible'), { path: '/' });
+        var uri = $(this).attr('href');
+        $.ajax({
+          url: uri,
+          success: function(data, dataType) {
+            $('#content').prepend(data);
+          }
+        });
+      }
+      return false;
+    });
+
+    $('#currentNaviConcept').on('click', function() {
+      $('#naviSites').hide();
+      $('#naviConcepts').toggle();
+      $.cookie("naviConceptsVisible", $('#naviConcepts').is(':visible'), { path: '/' });
+    });
   });
-});
+})(jQuery);
