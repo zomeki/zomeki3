91a5e14233b65e03e7086f3a5e6bca237396c53a
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2018-08-27 16:09:52 +0900

    [zomekiv3-spec:00255] Fix error on reorganization scheduled jobs

--- app/jobs/sys/reorg/exec_job.rb
+++ app/jobs/sys/reorg/exec_job.rb
@@ -2,13 +2,10 @@ class Sys::Reorg::ExecJob < ApplicationJob
   queue_as :default
   queue_with_priority 10
 
-  def perform(site)
-    schedule = Sys::Reorg::Schedule.where(site: site).first
-    return unless schedule
-
+  def perform(schedule)
     if schedule.state == 'reserved' && schedule.reserved_at && schedule.reserved_at <= Time.now
       schedule.update_columns(state: 'performing')
-      ::Script.run('sys/reorgs/exec', site_id: site.id)
+      ::Script.run('sys/reorgs/exec', site_id: schedule.site_id)
       schedule.update_columns(state: 'performed')
     end
   end
