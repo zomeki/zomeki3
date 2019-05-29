ff9425e39e4242df5c001199f8932611ba799e7f
mmukai <mukai@sitebridge.co.jp>
2019-05-29 12:01:06 +0900

    [zomekiv3-sqr:01142] Fix to save value in body when using template in article

--- lib/gp_template/model/rel/template.rb
+++ lib/gp_template/model/rel/template.rb
@@ -6,6 +6,7 @@ module GpTemplate::Model::Rel::Template
     belongs_to :template, class_name: 'GpTemplate::Template'
     after_initialize :set_template_defaults
     before_validation :convert_template_values
+    before_save :set_template_values_to_body
   end
 
   private
@@ -18,4 +19,15 @@ module GpTemplate::Model::Rel::Template
     raw_value = self.read_attribute_before_type_cast(:template_values)
     self.template_values = raw_value.to_h.with_indifferent_access if raw_value.is_a?(ActionController::Parameters)
   end
+  
+  def set_template_values_to_body
+    return unless self.template
+    return unless self.respond_to?(:body) && self.respond_to?(:files)
+    template_body = ''
+    template.items.each do |item|
+      template_body += ApplicationController.helpers.template_item_value(item, template_values[item.name].to_s, self.files)
+    end
+    self.body = template_body
+  end
+
 end
