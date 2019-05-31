d3b2d338d2c06b8df11a5488a858697eb743834d
mmukai <mukai@sitebridge.co.jp>
2019-05-31 16:07:59 +0900

    [zomekiv3-sqr:01164] Fixed behavior when selecting template for article

--- lib/gp_template/model/rel/template.rb
+++ lib/gp_template/model/rel/template.rb
@@ -6,7 +6,7 @@ module GpTemplate::Model::Rel::Template
     belongs_to :template, class_name: 'GpTemplate::Template'
     after_initialize :set_template_defaults
     before_validation :convert_template_values
-    before_save :set_template_values_to_body
+    after_save :set_template_values_to_body
   end
 
   private
@@ -23,11 +23,8 @@ module GpTemplate::Model::Rel::Template
   def set_template_values_to_body
     return unless self.template
     return unless self.respond_to?(:body) && self.respond_to?(:files)
-    template_body = ''
-    template.items.each do |item|
-      template_body += ApplicationController.helpers.template_item_value(item, template_values[item.name].to_s, self.files)
-    end
-    self.body = template_body
+    template_body = ApplicationController.helpers.template_body(self.template, self.template_values, self.files)
+    self.update_column(:body, template_body)
   end
 
 end
