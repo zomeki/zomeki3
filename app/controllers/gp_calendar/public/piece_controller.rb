8223d1545027ebe9893648b56beeda3c6c220d8b
mmukai <mukai@sitebridge.co.jp>
2019-06-06 16:37:32 +0900

    [zomekiv3-sqr:01144] Fixed date checks on pieces of calendar content

--- app/controllers/gp_calendar/public/piece_controller.rb
+++ app/controllers/gp_calendar/public/piece_controller.rb
@@ -29,6 +29,7 @@ class GpCalendar::Public::PieceController < Cms::Controller::Public::Piece
 
     paths = params[:path].split('/')
     return if paths.size < 2
+    return unless paths[0] =~ /^[0-9]{4}$/ && paths[1] =~ /^[0-9]{1,2}$/
 
     params[:year], params[:month] = paths[0], paths[1]
   end
