c46d38ef5816690ccad560edea85a614e9228a9b
mmukai <mukai@sitebridge.co.jp>
2019-07-19 11:41:31 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_31_084651) do
+ActiveRecord::Schema.define(version: 2019_07_17_143908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 2018_10_31_084651) do
     t.integer "thumb_size"
     t.text "alt_text"
     t.string "sp_url"
+    t.string "nofollow"
+    t.string "lazyload"
     t.index ["token"], name: "index_ad_banner_banners_on_token"
   end
 
