e20d90113f26deccf7d928bb7c4d67319a89d3c0
mmukai <mukai@sitebridge.co.jp>
2019-08-23 11:44:37 +0900

    [zomekiv3-sqr:01245] Limit the maximum attachment size of Survey::Question

--- db/migrate/20190822152832_update_form_file_max_size_on_survey_questions.rb
+++ db/migrate/20190822152832_update_form_file_max_size_on_survey_questions.rb
@@ -1,6 +1,6 @@
 class UpdateFormFileMaxSizeOnSurveyQuestions < ActiveRecord::Migration[5.0]
   def up
-    Survey::Question.where('form_file_max_size > 10').all.each do |question|
+    Survey::Question.where('form_file_max_size > 10').each do |question|
       question.update_column(:form_file_max_size, 10)
     end
   end
