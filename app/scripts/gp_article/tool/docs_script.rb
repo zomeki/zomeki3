e31e00b9c4ca8153543bd7bef56255db2a06656f
mmukai <mukai@sitebridge.co.jp>
2019-11-25 11:36:48 +0900

    [zomekiv3-sqr:01165] Added article reconstruction button to template details screen

--- app/scripts/gp_article/tool/docs_script.rb
+++ app/scripts/gp_article/tool/docs_script.rb
@@ -2,28 +2,28 @@ class GpArticle::Tool::DocsScript < ParametersScript
   def rebuild
     if params[:doc_id]
       doc_ids = GpArticle::Doc.public_state.where(id: params[:doc_id]).pluck(:id)
-      doc_ids.each_slice(100) do |sliced_doc_ids|
-        GpArticle::Doc.public_state.where(id: sliced_doc_ids).each do |doc|
-          ::Script.progress(doc) do
-            doc.rebuild
-          end
-        end
-      end
+      rebuild_doc(doc_ids)
     else
       content = GpArticle::Content::Doc.find(params[:content_id])
       return unless content
       return unless content.public_node
   
       doc_ids = content.docs.public_state.order(content.docs_order_as_hash).pluck(:id)
-      doc_ids.each_slice(100) do |sliced_doc_ids|
-        content.docs.public_state.where(id: sliced_doc_ids).each do |doc|
-          ::Script.progress(doc) do
-            doc.rebuild
-          end
-        end
-      end
-  
+      rebuild_doc(doc_ids)
       Cms::NodesScript.new(target_node_id: content.public_nodes.pluck(:id)).publish if content.public_nodes.present?
     end
   end
+  
+  private
+  
+  def rebuild_doc(doc_ids)
+    doc_ids.each_slice(100) do |sliced_doc_ids|
+      GpArticle::Doc.public_state.where(id: sliced_doc_ids).each do |doc|
+        ::Script.progress(doc) do
+          doc.rebuild
+        end
+      end
+    end
+  end
+  
 end
